0e2345c3141b2d5a447101fff8243331af44e04b
ShinZ3PHYR <florian.zephyr.allard@gmail.com>
2020-07-11 13:54:31 +0200

    Screen check and spellCheck

--- Assets/Scripts/Objects/Center.cs
+++ Assets/Scripts/Objects/Center.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class Center : MonoBehaviour
 {
-
+    
     private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            other.gameObject.SetActive(false);
+        if (Input.anyKeyDown)
+        {        
+            string keyPressed = Input.inputString;
+
+            if (Input.inputString == GameManager.singleton.TextPickerManager.currentChar)
+            {
+                //GameManager.singleton.TextPickerManager.ColorizeCharGreen();
+                GameManager.singleton.TextPickerManager.GetNextChar();
+                other.gameObject.SetActive(false);
+            }
+            else
+            {
+                //life -- 
+                CameraController.instance.ScreenShake(0.2f, 0.15f);
+            }
         }
     }
 }
